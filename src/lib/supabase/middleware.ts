6c3b884279f33183913b1439677101d784fcce82
DarleneQing <ophlia.cn@gmail.com>
2025-12-15 23:16:19 +0100

    middleware cookie setting

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -17,9 +17,6 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll()
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
-          )
           response = NextResponse.next({
             request: {
               headers: request.headers,
